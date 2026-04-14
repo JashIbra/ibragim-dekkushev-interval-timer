a975d03658a4f23e4c40ef6b290123f0384146dc
IbrahimDekkush <jashibrash@mail.ru>
2026-04-14 21:18:29 +0200

    [Hilt] Setup Hilt and Network configuration
    - Add `IntervalTimerApplication` class
    - Create `NetworkModule` to provide `Retrofit`, `OkHttpClient`, and `Json` serialization instances.
    - Add `INTERNET` permission and set `android:name` in `AndroidManifest.xml`.
    - Enable AndroidX and Jetifier in `gradle.properties`.

--- app/src/main/java/com/ibragimdekkushev/intervaltimer/MainActivity.kt
+++ app/src/main/java/com/ibragimdekkushev/intervaltimer/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.ibragimdekkushev.intervaltimer.ui.theme.IntervalTimerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/ibragimdekkushev/intervaltimer/di/NetworkModule.kt
@@ -0,0 +1,68 @@
+package com.ibragimdekkushev.intervaltimer.di
+
+import com.ibragimdekkushev.intervaltimer.BuildConfig
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptor(): Interceptor = Interceptor { chain ->
+        val request = chain.request().newBuilder()
+            .addHeader("App-Token", BuildConfig.APP_TOKEN)
+            .addHeader("Authorization", "Bearer ${BuildConfig.AUTH_TOKEN}")
+            .build()
+        chain.proceed(request)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        authInterceptor: Interceptor,
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(authInterceptor)
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        json: Json,
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .build()
+}
